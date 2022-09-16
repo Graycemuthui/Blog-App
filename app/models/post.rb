78aa5099288f0076f12d6496c2421ce125653bb4
Grayce Muthui <graycemuthui20@gmail.com>
2022-09-16 18:49:37 +0300

    Add validation to user and posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,9 +3,13 @@ class Post < ApplicationRecord
   has_many :comments, foreign_key: 'post_id'
   has_many :likes, foreign_key: 'post_id'
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def self.post_counter(author_id)
     user = User.find(author_id)
-    user.post_counter += 1
+    user.posts_counter += 1
     user.save
   end
 
