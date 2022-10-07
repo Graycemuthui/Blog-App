cefbbfc7800d921db8db00f2441418897a87b9ce
Grayce Muthui <graycemuthui20@gmail.com>
2022-10-07 04:43:21 +0300

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show new create] do
       resources :comments, only: [:create]
       resources :likes, only: [:create]
+      delete 'logout', to: 'sessions#destroy'
     end
   end
 
